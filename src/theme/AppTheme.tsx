ab16ef23317b3690c1fb0e3c5322d0aea4626279
juan manitta <manittajuan@gmail.com>
2023-02-20 19:17:14 -0300

    version de prueba

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -1,9 +1,7 @@
 import { ThemeProvider } from "@emotion/react"
 import { ReactNode } from "react";
-import { mainTheme, darkTheme } from './mainTheme';
 import { CssBaseline, createTheme } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { colorMode } from '../features/dark-mode/colorModeSlice';
 import { getDesignTokens } from "./getDesignThemeTokens";
 
 
